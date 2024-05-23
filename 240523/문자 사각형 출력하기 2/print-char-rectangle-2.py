1c2402dfaadd001a54b02365d2d92c0ccb3d704c
JunSung Kim <47361215+JUNSUNGKIM99@users.noreply.github.com>
2024-05-23 20:43:02 +0900

    Title: 문자 사각형 출력하기 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240523/문자 사각형 출력하기 2/print-char-rectangle-2.py
@@ -0,0 +1,25 @@
+def print_square(n):
+    # 알파벳 리스트 생성
+    alphabet = [chr(ord('A') + i) for i in range(26)]
+    # 출력할 격자 생성
+    grid = [[' ' for _ in range(n)] for _ in range(n)]
+
+    # 문자 채우기
+    char_index = 0
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = alphabet[char_index]
+            char_index = (char_index + 1) % 26
+
+    # 격자 출력
+    for i in range(n):
+        for j in range(n):
+            print(grid[i][j], end=' ')
+        print()
+    
+def main():
+    n = int(input())
+    print_square(n)
+
+if __name__ == "__main__":
+    main()
