1fccc47827cf8cc9bf09e07b1d28e03281a8b790
JunSung Kim <47361215+JUNSUNGKIM99@users.noreply.github.com>
2024-05-23 19:56:58 +0900

    Title: 순서대로 구구단, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240523/순서대로 구구단/multiplication-table-in-order.py
+++ 240523/순서대로 구구단/multiplication-table-in-order.py
@@ -1,4 +1,4 @@
 a, b = map(int, input().split())
 
 for i in range(1, 10):
-    print("{} * {} = {} {} * {} = {}".format(a,i,a*i,b,i,b*i))
+    print("{} * {} = {}  {} * {} = {}".format(a,i,a*i,b,i,b*i))
