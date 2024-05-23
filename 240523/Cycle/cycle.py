eff051ea9ebbf7bccd0310fdea8abbd654ed7e3f
JunSung Kim <47361215+JUNSUNGKIM99@users.noreply.github.com>
2024-05-23 20:55:16 +0900

    Title: Cycle, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240523/Cycle/cycle.py
+++ 240523/Cycle/cycle.py
@@ -4,10 +4,10 @@ backup = n
 number = set() 
 while True:
     n = (n*backup) % p
-    #print(i, n, number)
+    print(i, n, number)
     if n not in number:
         number.add(n)
         i += 1
     else:
         break
-print(i-1)
+print(i)
