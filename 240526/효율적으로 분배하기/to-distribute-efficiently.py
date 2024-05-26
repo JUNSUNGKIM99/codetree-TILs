176a9da55878f57afee8d038b4cf923b893a0676
JunSung Kim <47361215+JUNSUNGKIM99@users.noreply.github.com>
2024-05-26 16:25:51 +0900

    Title: 효율적으로 분배하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240526/효율적으로 분배하기/to-distribute-efficiently.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+answer = 0
+if n % 5 == 0:
+    print(n // 5)
+
+else:
+    while True:
+        n -= 3
+        answer += 1
+        if n % 5 == 0:
+            answer += n // 5
+            print(answer)
+            break
+        else:
+            continue
+        
+        if n < 0:
+            print(answer)
+            break
