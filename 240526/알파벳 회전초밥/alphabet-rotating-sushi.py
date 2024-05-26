253cfd603e1d216abf9bb7c661227cc1c39c7551
JunSung Kim <47361215+JUNSUNGKIM99@users.noreply.github.com>
2024-05-26 17:26:59 +0900

    Title: 알파벳 회전초밥, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240526/알파벳 회전초밥/alphabet-rotating-sushi.py
@@ -0,0 +1,15 @@
+tmp = input()
+word = input()
+position = []
+
+for i in range(len(word)-1):
+    if tmp.find(word[i]) < tmp.find(word[i+1]):
+        position.appned(1)
+    else:
+        position.append(0)
+answer = 1
+for p in position:
+    if p == 0:
+        answer += 1
+
+print(answer)
