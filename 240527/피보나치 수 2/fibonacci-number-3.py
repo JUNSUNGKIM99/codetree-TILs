dc6fbd71a6960755c9258f905d31ee75658b4ab8
JunSung Kim <47361215+JUNSUNGKIM99@users.noreply.github.com>
2024-05-27 01:06:21 +0900

    Title: 피보나치 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240527/피보나치 수 2/fibonacci-number-3.py
+++ 240527/피보나치 수 2/fibonacci-number-3.py
@@ -1,15 +1,15 @@
-def fibonacci(n):
+import sys
+sys.setrecursionlimit(1500)
+
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
     if n == 0:
         return 0
-    elif n == 1:
+    if n == 1:
         return 1
-    
-    dp = [0] * (n + 1)
-    dp[1] = 1
-    
-    for i in range(2, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
-    
-    return dp[n]
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+    return memo[n]
+
 n = int(input())
 print(fibonacci(n))
